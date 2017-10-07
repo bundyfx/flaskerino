dc88651adef103ecde8dcec2975b5fa2030e5001
Flynn Bundy <f.bundy@coolblue.nl>
2017-10-07 20:39:55 +0200

    Updating deployment scripts

--- deployment/application_stop.sh
+++ deployment/application_stop.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-PID=$(ps aux | grep /usr/bin/python3.5 | grep -v grep | awk '{print $2}')
+PID=$(ps aux | grep /usr/local/bin/flask | grep -v grep | awk '{print $2}')
 
 if [ -z "$PID" ];
 then
     exit 0
 else
-    kill $PID
+    kill $PID >/dev/null 2>&1
+    exit 0
 fi
 
