7730ad72471f2b7a096a12d24f1fb370ad0e5d83
Flynn Bundy <f.bundy@coolblue.nl>
2017-10-07 16:35:02 +0200

    Updating deployment scripts

--- deployment/userdata.sh
+++ deployment/userdata.sh
@@ -3,8 +3,8 @@ yum -y update
 yum install epel-release -y
 yum install ruby -y
 yum install python35 -y
-alias python='/usr/bin/python3.5'
-pip install flask pylint
+yum install python35-pip -y
+pip-3.5 install flask pylint
 cd /home/ec2-user
 curl -O https://aws-codedeploy-eu-west-1.s3.amazonaws.com/latest/install
 chmod +x ./install
