e77886d383ce6ea150d809f6d7c9ea5ec1ca0cc9
Flynn Bundy <f.bundy@coolblue.nl>
2017-10-07 18:16:53 +0200

    Updating deployment scripts

--- deployment/application_start.sh
+++ deployment/application_start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 export FLASK_APP=$(find "$(pwd -P)" -name "flaskerino.py")
-/usr/local/bin/flask run --host=0.0.0.0 &
-
-if [ $? == 0 ]; then
-    exit 0
-else
-    exit 1
-fi
+/usr/local/bin/flask run --host=0.0.0.0 >/dev/null 2>&1 &
