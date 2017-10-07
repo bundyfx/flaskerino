bd5ef2ded84ed0ff21939ad8c8ece91960b93cce
Flynn Bundy <f.bundy@coolblue.nl>
2017-10-07 21:36:35 +0200

    Updating deployment scripts

--- deployment/application_stop.sh
+++ deployment/application_stop.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-exit 0
+PID=$(ps aux | grep /usr/local/bin/flask | grep -v grep | awk '{print $2}')
+
+re='^[0-9]+([.][0-9]+)?$'
+if ! [[ $PID =~ $re ]] ; then
+    exit 0
+else
+    kill $PID >/dev/null 2>&1
+    exit 0
+fi
 
--- deployment/validate.sh
+++ deployment/validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 status_code=$(curl --silent --head 0.0.0.0:5000/index | grep HTTP/ | awk -F ' ' '{ print $2 }')
 
-if [ $status_code == "200" ]; then
+if [[ $status_code == "200" ]]; then
     exit 0
 else
     exit 1
