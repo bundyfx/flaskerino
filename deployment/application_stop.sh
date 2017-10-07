4a11d710e47767e589c748221eac683488938fda
Flynn Bundy <f.bundy@coolblue.nl>
2017-10-07 21:51:45 +0200

    Updating deployment scripts

--- deployment/application_stop.sh
+++ deployment/application_stop.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-PID=$(ps aux | grep /usr/local/bin/flask | grep -v grep | awk '{print $2}')
-
-re='^[0-9]+([.][0-9]+)?$'
-if ! [[ $PID =~ $re ]] ; then
-    exit 0
-else
-    kill $PID >/dev/null 2>&1
-    exit 0
-fi
-
+exit 0
