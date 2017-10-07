d693c14cd43b701012f13df61b5f843b136c3664
Flynn Bundy <f.bundy@coolblue.nl>
2017-10-07 17:30:06 +0200

    Updating deployment scripts

--- deployment/validate.sh
+++ deployment/validate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-status_code=$(curl --silent --head 127.0.0.1:5000/index | grep HTTP/ | awk -F ' ' '{ print $2 }')
+status_code=$(curl --silent --head 0.0.0.0:5000/index | grep HTTP/ | awk -F ' ' '{ print $2 }')
 
 if [ $status_code == "200" ]; then
     exit 0
